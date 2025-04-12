5401bf79aace0dc8144f319a4d3e035945dfd5d5
HCKR_JK <95833910+ShreyHackerStorm@users.noreply.github.com>
2025-04-12 15:50:30 -0700

    UI(mostly Done)

--- febhacks_AkhilandShreyas/demo/myapp/urls.py
+++ febhacks_AkhilandShreyas/demo/myapp/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.set_allowance, name='set_allowance'),
+    path('set-allowance/', views.set_allowance, name='set_allowance'),
     path('add-expenses/', views.add_expenses, name='add_expenses'),
     path('view-spending/', views.view_spending, name='view_spending'),
+    path()
 ]
